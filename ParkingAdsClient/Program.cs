dcb1ec4cf9029e842f5e579f7e7122077d67b8bb
Peanguin <Peanguin@users.noreply.github.com>
2021-12-20 01:20:30 +0100

    Add project files.

---
+++ ParkingAdsClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParkingAdsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
