60c1e632c482f14d26c265a6cc983704b84f20d5
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-01-05 13:21:39 +0100

    redis get and save method

--- Service_Solution/Service_Solution_Project2_PBA/Service_Solution_Project2_PBA/Program.cs
+++ Service_Solution/Service_Solution_Project2_PBA/Service_Solution_Project2_PBA/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Service_Solution_Project2_PBA
 {
@@ -8,5 +11,45 @@ namespace Service_Solution_Project2_PBA
         {
             Console.WriteLine("Hello World!");
         }
+    }
+
+    class ConnectionToRedis
+    {
+        const string instance_Name = "RedisDemo_";
+        const string connString = "localhost:5002"; //Mapped the redis port to docker. 
+    }
+
+    static class DistributedCacheExtensions
+    {
+        //save item method
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache, 
+            string recordID,                        //the key/unique identifier
+            T data,                                 //whatever we store in the cache
+            TimeSpan? absoluteExpireTime = null,    //setting null as default
+            TimeSpan? unusedExpireTime = null)  
+        {
+            var options = new DistributedCacheEntryOptions();           //setup the options for the entries we put in the cache.
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60); //?? = if null use 60 secs
+            options.SlidingExpiration = unusedExpireTime ?? TimeSpan.FromSeconds(3600);                 //Not accesing for 1 hour.
+
+            var jsonData = JsonSerializer.Serialize(data);  //The entry to Json data format.
+            await cache.SetStringAsync(recordID, jsonData, options);
+        }
+        //get item method
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+            if (jsonData is null)
+            {
+                return default(T);  //return the default value of an entry we pass in if not found. An object = null, int = 0 etc...
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+
+    class MessageHandler
+    {
+
     }
 }
