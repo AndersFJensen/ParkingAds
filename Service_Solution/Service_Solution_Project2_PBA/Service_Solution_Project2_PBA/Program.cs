e3cd8b2f4eeba84eb74046b50d0c0d47fe5fc346
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-01-05 10:53:02 +0100

    Service solution init

---
+++ Service_Solution/Service_Solution_Project2_PBA/Service_Solution_Project2_PBA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Service_Solution_Project2_PBA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
