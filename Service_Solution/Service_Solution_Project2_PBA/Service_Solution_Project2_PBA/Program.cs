c08753394b08e58dc1f1a5bf4f11cb11c67ba6be
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-01-07 12:55:57 +0100

    Its working!

--- Service_Solution/Service_Solution_Project2_PBA/Service_Solution_Project2_PBA/Program.cs
+++ Service_Solution/Service_Solution_Project2_PBA/Service_Solution_Project2_PBA/Program.cs
@@ -3,16 +3,20 @@ using System;
 using System.Threading.Tasks;
 using System.Text.Json;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
+using System.Text.Json.Serialization;
 
 namespace Service_Solution_Project2_PBA
 {
     class Program
     {
-        private struct Entry
+        [Serializable]
+        private struct TestEntry
         {
+            [JsonInclude]
             public string id;
+            [JsonInclude]
             public string desc; 
-            public Entry(string id, string desc)
+            public TestEntry(string id, string desc)
             {
                 this.id = id;
                 this.desc = desc; 
@@ -21,52 +25,49 @@ namespace Service_Solution_Project2_PBA
 
         static async Task Main(string[] args)
         {
-            RedisCache redisCache = new RedisCache(new MyRedisConfigOptions());
-            var TestEntry1 = new Entry("123", "It is working!");
-   
+            RedisCache redisCache = new RedisCache(new RedisCacheOptions
+            {
+                Configuration = "127.0.0.1:5002", 
+                InstanceName = "RedisCache_" + Guid.NewGuid().ToString(),
+            });
+            var TestEntry1 = new TestEntry("123", "It is working!");
             await DistributedCacheExtensions.SetRecordAsync(redisCache, TestEntry1.id, TestEntry1);
-            var result = await DistributedCacheExtensions.GetRecordAsync<Entry>(redisCache, TestEntry1.id);
-            Console.WriteLine($"Entry {result} was found! \n");
+            var result = await DistributedCacheExtensions.GetRecordAsync<TestEntry>(redisCache, TestEntry1.id);
+            if(!result.Equals(null))
+            Console.WriteLine($"Entry with {result.id} and discription '{result.desc}' was found! \n");
+            else
+            { Console.WriteLine("Not working!"); }
         }
-    }
-
-    public class MyRedisConfigOptions : RedisCacheOptions
-    {
-        public RedisCacheOptions value = new RedisCacheOptions
-        {
-            Configuration = "127.0.0.1:5002",
-            InstanceName = "RedisDemo_"
-        };
     }
 
     public static class DistributedCacheExtensions
     {
         /**save entry to Redis method*/
         public static async Task SetRecordAsync<T>(this IDistributedCache cache, 
-            string recordID,                        //the key/unique identifier
-            T data,                                 //whatever we store in the cache
-            TimeSpan? absoluteExpireTime = null,    //setting null as default
+            string recordID,                       
+            T data,                                
+            TimeSpan? absoluteExpireTime = null,    
             TimeSpan? unusedExpireTime = null)  
         {
-            var options = new DistributedCacheEntryOptions();           //setup the options for the entries we put in the cache.
-            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60); //?? = if null use 60 secs
-            options.SlidingExpiration = unusedExpireTime ?? TimeSpan.FromSeconds(3600);                 //Not accesing for 1 hour.
-          
-            var jsonData = JsonSerializer.Serialize(data);  //The entry to Json data format.
+            var options = new DistributedCacheEntryOptions();           
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60); 
+            options.SlidingExpiration = unusedExpireTime ?? TimeSpan.FromSeconds(3600);                 
+     
+            var jsonData = JsonSerializer.Serialize(data); 
             await cache.SetStringAsync(recordID, jsonData, options);
-            Console.WriteLine($"Saved entry to Redis with id {recordID} at {DateTime.Now} \n");
+            Console.WriteLine($"Saved entry {jsonData} to Redis with id {recordID} at {DateTime.Now} \n");
         }
 
         /**get entry from Redis method*/
         public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
         {
-            var jsonData = await cache.GetStringAsync(recordId);
+            string jsonData = await cache.GetStringAsync(recordId);
             if (jsonData is null)
             {
-                return default(T);  //return the default value of an entry we pass in if not found. An object = null, int = 0 etc...
+                return default(T);  
             }
 
-            Console.WriteLine($"Loaded entry from Redis with id {recordId} at {DateTime.Now} \n");
+            Console.WriteLine($"Loaded entry {jsonData} from Redis with id {recordId} at {DateTime.Now} \n");
             return JsonSerializer.Deserialize<T>(jsonData);
         }
     }
