e0ef8eedbc0199847b8bb8d49ff70795cfbfbc24
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-01-07 13:47:27 +0100

    Added more test entries

--- Service_Solution/Service_Solution_Project2_PBA/Service_Solution_Project2_PBA/Program.cs
+++ Service_Solution/Service_Solution_Project2_PBA/Service_Solution_Project2_PBA/Program.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
 using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Service_Solution_Project2_PBA
 {
@@ -28,13 +30,27 @@ namespace Service_Solution_Project2_PBA
             RedisCache redisCache = new RedisCache(new RedisCacheOptions
             {
                 Configuration = "127.0.0.1:5002", 
-                InstanceName = "RedisCache_" + Guid.NewGuid().ToString(),
+                //InstanceName = "RedisCache_" + Guid.NewGuid().ToString(),
             });
             var TestEntry1 = new TestEntry("123", "It is working!");
+            var TestEntry2 = new TestEntry("1234", "It is workingx!");
+            var TestEntry3 = new TestEntry("12345", "It is workingy!");
+            var TestEntry4 = new TestEntry("123456", "It is workingz!");
             await DistributedCacheExtensions.SetRecordAsync(redisCache, TestEntry1.id, TestEntry1);
-            var result = await DistributedCacheExtensions.GetRecordAsync<TestEntry>(redisCache, TestEntry1.id);
-            if(!result.Equals(null))
-            Console.WriteLine($"Entry with {result.id} and discription '{result.desc}' was found! \n");
+            await DistributedCacheExtensions.SetRecordAsync(redisCache, TestEntry2.id, TestEntry2);
+            await DistributedCacheExtensions.SetRecordAsync(redisCache, TestEntry3.id, TestEntry3);
+            await DistributedCacheExtensions.SetRecordAsync(redisCache, TestEntry4.id, TestEntry4);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            List<TestEntry> list = new List<TestEntry>();
+            list.Add(await DistributedCacheExtensions.GetRecordAsync<TestEntry>(redisCache, TestEntry1.id));
+            list.Add(await DistributedCacheExtensions.GetRecordAsync<TestEntry>(redisCache, TestEntry2.id));
+            list.Add(await DistributedCacheExtensions.GetRecordAsync<TestEntry>(redisCache, TestEntry3.id));
+            list.Add(await DistributedCacheExtensions.GetRecordAsync<TestEntry>(redisCache, TestEntry4.id));
+            if (list.Count != 0)
+                foreach (TestEntry result in list)
+                {
+                    Console.WriteLine($"Entry with {result.id} and discription '{result.desc}' was found! \n");
+                }
             else
             { Console.WriteLine("Not working!"); }
         }
