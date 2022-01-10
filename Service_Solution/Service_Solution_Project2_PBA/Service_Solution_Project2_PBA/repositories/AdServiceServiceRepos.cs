d66b2b7513d11b7d8afd5caf7e1afa11dd14dd27
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-01-10 11:44:35 +0100

    Added AdService caller class. Need testing.

---
+++ Service_Solution/Service_Solution_Project2_PBA/Service_Solution_Project2_PBA/repositories/AdServiceServiceRepos.cs
@@ -0,0 +1,28 @@
+﻿using Service_Solution_Project2_PBA.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Service_Solution_Project2_PBA.repositories
+{
+    class AdServiceServiceRepos : AdServiceServiceReposIF
+    {
+        static HttpClient client = new HttpClient();
+        private const string url = "http://psuaddservice.fenris.ucn.dk/";
+        public async Task<Message> CallAdServiceGET()
+        {
+            Message message = null; 
+            HttpResponseMessage response = await client.GetAsync(url);
+            message.header = "AdService"; 
+            if (response.IsSuccessStatusCode)
+            {
+                message.body = await response.Content.ReadAsStringAsync();
+            }
+            return message; 
+        }
+    }
+}
