ee61522b8f3fe93abd2634ccbfc98facb66c92d8
Magnus <55579436+Sk41d@users.noreply.github.com>
2022-01-11 09:41:44 +0100

    Alot of files added for solution + booking service

---
+++ Service_Solution/Service_Solution_Project2_PBA/BookingService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
